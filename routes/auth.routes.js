2cae742ba89c916fa26d6ba46d7d4d4b9b185bbc
Murat <murat@Murats-Air.home>
2020-09-27 21:24:06 -0400

    new user registration

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 const {
     Router
 } = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 const router = Router();
 
 router.post('/signup', async (req, res) => {
@@ -9,9 +11,35 @@ router.post('/signup', async (req, res) => {
             email,
             password
         } = req.body;
+
+        // checking email
+        const condidate = await User.findOne({
+            email
+        })
+
+        if (condidate) {
+            return res.status(400).json({
+                message: 'User exists'
+            })
+        }
+
+        // hashing password
+        const hashedPassword = await bcrypt.hash(password, 15);
+
+        // creating user
+        const user = new User({
+            email,
+            password: hashedPassword
+        })
+
+        await user.save();
+
+        res.status(201).json({
+            message: 'User created'
+        });
     } catch (e) {
         res.status(500).json({
-            messge: "Something went wrong try again!"
+            message: "Something went wrong try again!"
         })
     }
 })
