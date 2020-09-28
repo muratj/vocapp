56229358c0d6710001d75d959fb4ea59a0cde8f5
Murat <murat@Murats-Air.home>
2020-09-27 21:03:08 -0400

    User scheme

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,5 +3,21 @@ const {
 } = require('express');
 const router = Router();
 
+router.post('/signup', async (req, res) => {
+    try {
+        const {
+            email,
+            password
+        } = req.body;
+    } catch (e) {
+        res.status(500).json({
+            messge: "Something went wrong try again!"
+        })
+    }
+})
+
+router.post('/login', async (req, res) => {
+
+})
 
 module.exports = router;
